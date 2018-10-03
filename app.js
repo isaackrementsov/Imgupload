d3ad773d423169b6a36913ee2d0351b39708dc13
Kaiden Cook <kaidencook03@gmail.com>
2018-10-03 12:20:18 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Importing modules
 var LocalStrategy = require('passport-local').Strategy;
 var flash = require("connect-flash");
 var passport = require("passport");
@@ -16,14 +17,14 @@ var rateLimiter = require("express-rate-limit");
 app.use(helmet({
   frameguard: {action: "deny"}
 }));
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000); //Better to set in separate function
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(cookieParser());
-mongoose.connect("mongodb://127.0.0.1:27017/users");
+mongoose.connect("mongodb://127.0.0.1:27017/users"); //Starting mongoDB
 app.listen(app.get('port'), function(){
   console.log("server started");
 });
@@ -65,4 +66,4 @@ var limiter = new rateLimiter({
   delayMs: 1
 });
 app.use(limiter);
-routes(app);
+routes(app); //Better to create separate routes file for more cleanliness
